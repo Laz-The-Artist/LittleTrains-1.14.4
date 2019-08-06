51998d54ae8c39b2e2c3918332de3a66567d5043
Redstoneguy129 <45243401+Redstoneguy129@users.noreply.github.com>
2019-08-06 21:32:30 +0100

    Blocks
    Added Blocks

--- src/main/java/com/multiteam/littletrains/LittleTrains.java
+++ src/main/java/com/multiteam/littletrains/LittleTrains.java
@@ -51,6 +51,15 @@ public class LittleTrains {
 		@SubscribeEvent
 		public static void RegisterItems(final RegistryEvent.Register<Item> event) {
 			event.getRegistry().registerAll (
+					LTItemInit.CONTROL_BOX = new BlockItem(LTBlockInit.CONTROL_BOX, new Item.Properties().group(littletrainsitems)).setRegistryName(LTBlockInit.CONTROL_BOX.getRegistryName()),
+					LTItemInit.MODELLER_TABLE = new BlockItem(LTBlockInit.MODELLER_TABLE, new Item.Properties().group(littletrainsitems)).setRegistryName(LTBlockInit.MODELLER_TABLE.getRegistryName()),
+					LTItemInit.TIN_BLOCK = new BlockItem(LTBlockInit.TIN_BLOCK, new Item.Properties().group(littletrainsitems)).setRegistryName(LTBlockInit.TIN_BLOCK.getRegistryName()),
+					LTItemInit.COPPER_BLOCK = new BlockItem(LTBlockInit.COPPER_BLOCK, new Item.Properties().group(littletrainsitems)).setRegistryName(LTBlockInit.COPPER_BLOCK.getRegistryName()),
+					LTItemInit.TIN_ORE = new BlockItem(LTBlockInit.TIN_ORE, new Item.Properties().group(littletrainsitems)).setRegistryName(LTBlockInit.TIN_ORE.getRegistryName()),
+					LTItemInit.COPPER_ORE = new BlockItem(LTBlockInit.COPPER_ORE, new Item.Properties().group(littletrainsitems)).setRegistryName(LTBlockInit.COPPER_ORE.getRegistryName()),
+					LTItemInit.COBBLE_ANDESITE = new BlockItem(LTBlockInit.COBBLE_ANDESITE, new Item.Properties().group(littletrainsitems)).setRegistryName(LTBlockInit.COBBLE_ANDESITE.getRegistryName()),
+					LTItemInit.COBBLE_HARDENED_CLAY = new BlockItem(LTBlockInit.COBBLE_HARDENED_CLAY, new Item.Properties().group(littletrainsitems)).setRegistryName(LTBlockInit.COBBLE_HARDENED_CLAY.getRegistryName()),
+					LTItemInit.BALLAST_BLOCK = new BlockItem(LTBlockInit.BALLAST_BLOCK, new Item.Properties().group(littletrainsitems)).setRegistryName(LTBlockInit.BALLAST_BLOCK.getRegistryName()),
 					LTItemInit.debugblock = new BlockItem(LTBlockInit.debugblock, new Item.Properties().group(littletrainsitems)).setRegistryName(LTBlockInit.debugblock.getRegistryName()),
 					LTItemInit.debug = new Item(new Item.Properties().group(littletrainsitems)).setRegistryName(l("debug"))
 					);
@@ -58,6 +67,15 @@ public class LittleTrains {
 		@SubscribeEvent
 		public static void RegisterBlocks(final RegistryEvent.Register<Block> event) {
 			event.getRegistry().registerAll (
+					LTBlockInit.CONTROL_BOX = new Block(Block.Properties.create(Material.MISCELLANEOUS).sound(SoundType.METAL)).setRegistryName(l("control_box")),
+					LTBlockInit.MODELLER_TABLE = new Block(Block.Properties.create(Material.MISCELLANEOUS).sound(SoundType.METAL)).setRegistryName(l("modeller_table")),
+					LTBlockInit.COBBLE_ANDESITE = new Block(Block.Properties.create(Material.ROCK).sound(SoundType.STONE)).setRegistryName(l("cobble_andesite")),
+					LTBlockInit.COBBLE_HARDENED_CLAY = new Block(Block.Properties.create(Material.ROCK).sound(SoundType.STONE)).setRegistryName(l("cobble_hardened_clay")),
+					LTBlockInit.BALLAST_BLOCK = new Block(Block.Properties.create(Material.ROCK).sound(SoundType.STONE)).setRegistryName(l("ballast_block")),
+					LTBlockInit.COPPER_ORE = new Block(Block.Properties.create(Material.ROCK).sound(SoundType.STONE)).setRegistryName(l("copper_ore")),
+					LTBlockInit.TIN_ORE = new Block(Block.Properties.create(Material.ROCK).sound(SoundType.STONE)).setRegistryName(l("tin_ore")),
+					LTBlockInit.COPPER_BLOCK = new Block(Block.Properties.create(Material.IRON).sound(SoundType.STONE)).setRegistryName(l("copper_block")),
+					LTBlockInit.TIN_BLOCK = new Block(Block.Properties.create(Material.IRON).sound(SoundType.STONE)).setRegistryName(l("tin_block")),
 					LTBlockInit.debugblock = new Block(Block.Properties.create(Material.WOOD).lightValue(15).hardnessAndResistance(0.5F).sound(SoundType.SAND)).setRegistryName(l("debugblock"))
 					);
 		}
--- src/main/java/com/multiteam/littletrains/init/LTBlockInit.java
+++ src/main/java/com/multiteam/littletrains/init/LTBlockInit.java
@@ -4,5 +4,18 @@ import net.minecraft.block.Block;
 
 public class LTBlockInit {
 	public static Block debug, debugblock;
+	
+	//Non-Special
+	public static Block TIN_ORE;
+	public static Block TIN_BLOCK;
+	public static Block COPPER_BLOCK;
+	public static Block COPPER_ORE;
+	public static Block COBBLE_HARDENED_CLAY;
+	public static Block COBBLE_ANDESITE;
+	public static Block BALLAST_BLOCK;
+	
+	//Special
+	public static Block CONTROL_BOX;
+	public static Block MODELLER_TABLE;
 
 }
